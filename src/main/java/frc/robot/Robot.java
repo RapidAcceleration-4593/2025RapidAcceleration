94e2659cb3f7d98f81789905a5542ee0f61ba754
tylerjacobmueller08@gmail.com <tylerjacobmueller08@gmail.com>
2025-02-03 17:38:48 -0700

    Updating venderdeps and restarting PhotonVision service directly

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -68,7 +68,6 @@ public class Robot extends TimedRobot {
         if (disabledTimer.hasElapsed(Constants.DrivebaseConstants.WHEEL_LOCK_TIME)) {
             m_robotContainer.setMotorBrake(false);
             disabledTimer.stop();
-            disabledTimer.reset();
         }
     }
 
