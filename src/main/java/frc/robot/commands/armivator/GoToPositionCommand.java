770a47172fae05d42a4ba42fe3109071f63679c2
Matthew Flanegan <mvoncrackersnackin@gmail.com>
2025-02-26 15:57:49 -0600

    fixed scheduling issues in GoToPositionCommand

--- src/main/java/frc/robot/commands/armivator/GoToPositionCommand.java
+++ src/main/java/frc/robot/commands/armivator/GoToPositionCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.armivator;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ArmConstants.ArmStates;
 import frc.robot.Constants.ElevatorConstants.ElevatorStates;
@@ -12,20 +13,27 @@ public class GoToPositionCommand extends SequentialCommandGroup {
     ElevatorSubsystem elevatorSubsystem;
     ArmSubsystem armSubsystem;
 
+    ElevatorStates targetElevator;
+    ArmStates targetArm;
+
 
     public GoToPositionCommand(ArmStates armState, ElevatorStates elevatorState, ElevatorSubsystem elevatorSubsystem, ArmSubsystem armSubsystem) {
         this.elevatorSubsystem = elevatorSubsystem;
         this.armSubsystem = armSubsystem;
 
-        handleKachunkCollision(armState, elevatorState);
-
+        this.targetElevator = elevatorState;
+        this.targetArm = armState;
+        
         addCommands(
-            elevatorSubsystem.GoToStateCommand(elevatorState),
-            armSubsystem.GoToStateCommand(armState)
+            elevatorSubsystem.GoToStateCommand(ElevatorStates.PICKUP).onlyIf(this::willCollide),
+            Commands.parallel(
+                elevatorSubsystem.GoToStateCommand(elevatorState),
+                armSubsystem.GoToStateCommand(armState)
+            )
         );
     }
 
-    private void handleKachunkCollision(ArmStates targetArm, ElevatorStates targetElevator) {
+    private boolean willCollide() {
         var isArmUpResult = armSubsystem.isUp(); // Arm may be uncertain if it is up
         if (isArmUpResult == IsUpResult.UNSURE) {
             addCommands(elevatorSubsystem.GoToStateCommand(ElevatorStates.PICKUP)); // Best be careful, the arm isn't sure if it is up or down
@@ -38,17 +46,15 @@ public class GoToPositionCommand extends SequentialCommandGroup {
         boolean targetArmUp = targetArm != ArmStates.BOTTOM;
         boolean targetElevatorUp = targetElevator != ElevatorStates.BOTTOM;
 
-        boolean requireIntakeState = false;
+        boolean requirePickupState = false;
                                                                                         // If any of these conditions are true, we need to go to INTAKE first
-        requireIntakeState |= !elevatorUp && !armUp && targetElevatorUp && targetArmUp; // 1. Elevator kachunked, and wants to go all the way up
-        requireIntakeState |= elevatorUp && armUp && !targetElevatorUp && !targetArmUp; // 2. The oppposite of 1.
+        requirePickupState |= !elevatorUp && !armUp && targetElevatorUp && targetArmUp; // 1. Elevator kachunked, and wants to go all the way up
+        requirePickupState |= elevatorUp && armUp && !targetElevatorUp && !targetArmUp; // 2. The oppposite of 1.
 
-        requireIntakeState |= !elevatorUp && !armUp && !targetElevatorUp && targetArmUp;// 3. Elevator kachunked, and wants to keep elevator down but arm out
-        requireIntakeState |= !elevatorUp && armUp && !targetElevatorUp && !targetArmUp;// 4. Elevator down, arm up, and wants to go to kachunk.
+        requirePickupState |= !elevatorUp && !armUp && !targetElevatorUp && targetArmUp;// 3. Elevator kachunked, and wants to keep elevator down but arm out
+        requirePickupState |= !elevatorUp && armUp && !targetElevatorUp && !targetArmUp;// 4. Elevator down, arm up, and wants to go to kachunk.
         
-        if (requireIntakeState) {
-            addCommands(elevatorSubsystem.GoToStateCommand(ElevatorStates.PICKUP)); // Best be careful, the arm isn't sure if it is up or down
-        }
+        return requirePickupState;
     }
     
 }
