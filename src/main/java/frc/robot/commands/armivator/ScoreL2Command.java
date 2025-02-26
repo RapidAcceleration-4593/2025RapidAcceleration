ec0551ca6c0ce399efcdb1857939647cdc5b2d12
Matthew Flanegan <mvoncrackersnackin@gmail.com>
2025-02-25 19:04:40 -0700

    added some code in score l2 that should be added to the new score command

--- src/main/java/frc/robot/commands/armivator/ScoreL2Command.java
+++ src/main/java/frc/robot/commands/armivator/ScoreL2Command.java
@@ -10,6 +10,21 @@ import frc.robot.subsystems.ElevatorSubsystem;
 public class ScoreL2Command extends SequentialCommandGroup {
 
     public ScoreL2Command(ElevatorSubsystem elevatorSubsystem, ArmSubsystem armSubsystem) {
+        ArmStates currentArmState = null;
+        ArmStates targetArmState = null;
+        ElevatorStates currentElevatorState = null;
+        ElevatorStates targetElevatorState = null;
+        
+        boolean currentlyKachunked = currentArmState == ArmStates.BOTTOM && currentElevatorState == ElevatorStates.BOTTOM;
+        boolean targetKachunked = targetArmState == ArmStates.BOTTOM && targetElevatorState == ElevatorStates.BOTTOM;
+
+        if (
+            (currentlyKachunked && targetArmState != ArmStates.BOTTOM) ||
+            (targetKachunked && currentArmState != ArmStates.BOTTOM)
+        ) {
+            addCommands(elevatorSubsystem.GoToStateCommand(ElevatorStates.PICKUP));
+        }
+
         addCommands(
             elevatorSubsystem.GoToStateCommand(ElevatorStates.PICKUP),
             armSubsystem.GoToStateCommand(ArmStates.L2),
