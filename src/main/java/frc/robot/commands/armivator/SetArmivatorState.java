9bb4e1046a308f657f7441bde2197113279ee082
TheCrackerSnacker <145797360+TheCrackerSnacker@users.noreply.github.com>
2025-03-07 20:43:45 -0700

    Reworked SetArmivatorState to be easier to read and less redundant. Hopefully it works.

--- src/main/java/frc/robot/commands/armivator/SetArmivatorState.java
+++ src/main/java/frc/robot/commands/armivator/SetArmivatorState.java
@@ -2,7 +2,6 @@ package frc.robot.commands.armivator;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.RobotStates.Arm.ArmDirections;
 import frc.robot.Constants.RobotStates.Arm.ArmStates;
 import frc.robot.Constants.RobotStates.Elevator.ElevatorStates;
@@ -11,13 +10,19 @@ import frc.robot.commands.elevator.SetElevatorState;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem;
 
-public class SetArmivatorState extends SequentialCommandGroup {
+public class SetArmivatorState extends Command {
+    private final ElevatorSubsystem elevatorSubsystem;
+    private final ArmSubsystem armSubsystem;
 
-    ElevatorSubsystem elevatorSubsystem;
-    ArmSubsystem armSubsystem;
+    private final ElevatorStates targetElevatorState;
+    private final ArmStates targetArmState;
 
-    ElevatorStates targetElevatorState;
-    ArmStates targetArmState;
+    private final Command pureSequentialSequence;
+    private final Command topToBottomSequence;
+    private final Command bottomToTopSequence;
+    private final Command pureParallelSequence;
+
+    private Command selectedSequence;
 
     public SetArmivatorState(ElevatorSubsystem elevatorSubsystem, ArmSubsystem armSubsystem, ElevatorStates elevatorState, ArmStates armState) {
         this.elevatorSubsystem = elevatorSubsystem;
@@ -26,99 +31,81 @@ public class SetArmivatorState extends SequentialCommandGroup {
         this.targetElevatorState = elevatorState;
         this.targetArmState = armState;
         
-        addCommands(
-            Commands.either(
-                Commands.either(
-                    handleCollisionCommand(),
-                    Commands.parallel(
-                        new SetElevatorState(elevatorSubsystem, elevatorState).withTimeout(1.25),
-                        new SetArmState(armSubsystem, armState).withTimeout(0.8)
-                    ),
-                    this::willCollide
-                ),
-                Commands.sequence(
-                    new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
-                    new SetArmState(armSubsystem, armState).withTimeout(0.8),
-                    new SetElevatorState(elevatorSubsystem, elevatorState).withTimeout(1.25)
-                ),
-                () -> armSubsystem.isArmUp() != ArmDirections.UNKNOWN
+        // Register commands (this way they only have to be created once)
+        pureSequentialSequence = Commands.sequence(
+            new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
+            new SetArmState(armSubsystem, targetArmState).withTimeout(0.8),
+            new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25)
+        );
+
+        topToBottomSequence = Commands.sequence(
+            Commands.parallel(
+                new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
+                new SetArmState(armSubsystem, targetArmState).withTimeout(0.8)
+            ),
+            new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25)
+        );
+
+        bottomToTopSequence = Commands.sequence(
+            new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
+            Commands.parallel(
+                new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25),
+                new SetArmState(armSubsystem, targetArmState).withTimeout(0.8)
             )
         );
-    }
 
-    private boolean willCollide() {
-        boolean armUp = armSubsystem.isArmUp() == ArmDirections.UP;
-        boolean elevatorUp = elevatorSubsystem.isElevatorUp();
-
-        boolean targetArmUp = targetArmState != ArmStates.BOTTOM;
-        boolean targetElevatorUp = targetElevatorState != ElevatorStates.BOTTOM;
-
-        boolean requirePickupState = false;
-                                                                                         // If any of these conditions are true, we need to go to INTAKE first.
-        requirePickupState |= !elevatorUp && !armUp && targetElevatorUp && targetArmUp;  // 1. Elevator kah-chunked and wants to go all the way up.
-        requirePickupState |= elevatorUp && armUp && !targetElevatorUp && !targetArmUp;  // 2. The opposite of 1.
-
-        requirePickupState |= !elevatorUp && !armUp && !targetElevatorUp && targetArmUp; // 3. Elevator kah-chunked and wants to keep elevator down but arm out.
-        requirePickupState |= !elevatorUp && armUp && !targetElevatorUp && !targetArmUp; // 4. Elevator down, arm up, and wants to go to kah-chunk.
-        
-        return requirePickupState;
-    }
-
-    /**
-     * Can the arm and elevator move in parallel, or must they be moved sequentially?
-     * Note that this method will return a nonsensical answer if willCollide returns false.
-     * @return Whether the arm and elevator can move to the target in parallel.
-     */
-    private boolean parallelSupported() {
-        boolean elevatorUp = elevatorSubsystem.isElevatorUp();
-        boolean targetElevatorUp = targetElevatorState != ElevatorStates.BOTTOM;
-
-        return elevatorUp || elevatorUp != targetElevatorUp;
-    }
-
-    /**
-     * Don't use this. It checks if the arm and elevator are currently down and are both moving to up.
-     * @return
-     */
-    private boolean isKahchunkToRaised () {
-        boolean armUp = armSubsystem.isArmUp() == ArmDirections.UP;
-        boolean elevatorUp = elevatorSubsystem.isElevatorUp();
-
-        boolean targetArmUp = targetArmState != ArmStates.BOTTOM;
-        boolean targetElevatorUp = targetElevatorState != ElevatorStates.BOTTOM;
-
-        return !elevatorUp && !armUp && targetElevatorUp && targetArmUp;
-    }
-
-    /**
-     * This command handles the nitty gritty details of what to do when the armivator has to avoid hitting the kahchunk block and bumper.
-     * @return The Command you call, when all is lost.
-     */
-    private Command handleCollisionCommand() {
-        return Commands.either(
-            Commands.either(
-                Commands.sequence(
-                    new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
-                    Commands.parallel(
-                        new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25),
-                        new SetArmState(armSubsystem, targetArmState).withTimeout(0.8)
-                    )
-                ),
-                Commands.sequence(
-                    Commands.parallel(
-                        new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
-                        new SetArmState(armSubsystem, targetArmState).withTimeout(0.8)
-                    ),
-                    new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25)
-                ),
-                this::isKahchunkToRaised
-            ),
-            Commands.sequence(
-                new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
-                new SetArmState(armSubsystem, targetArmState).withTimeout(0.8),
-                new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25)
-            ),
-            this::parallelSupported
+        pureParallelSequence = Commands.parallel(
+            new SetElevatorState(elevatorSubsystem, targetElevatorState).withTimeout(1.25),
+            new SetArmState(armSubsystem, targetArmState).withTimeout(0.8)
         );
+
+        addRequirements(elevatorSubsystem, armSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        selectedSequence = determineMovementSequence();
+        selectedSequence.initialize();
+    }
+
+    @Override
+    public void execute() {
+        selectedSequence.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        selectedSequence.end(interrupted);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return selectedSequence.isFinished();
+    }
+
+    private Command determineMovementSequence() {
+        boolean armUp = armSubsystem.isArmUp() == ArmDirections.UP;
+        boolean elevatorUp = elevatorSubsystem.isElevatorUp();
+
+        boolean targetArmUp = targetArmState != ArmStates.BOTTOM;
+        boolean targetElevatorUp = targetElevatorState != ElevatorStates.BOTTOM;
+
+        // This sequence is the most stable. Elevator moves to PICKUP, the arm goes to target, and then elevator moves to target.
+        // No matter what position the armivator is in, this sequence will always work.
+        if (armSubsystem.isArmUp() == ArmDirections.UNKNOWN ||          // If we don't know where the arm is, we'd best use the safest sequence possible
+            !elevatorUp && !targetElevatorUp && armUp != targetArmUp) { // Elevator down and wants to stay down but switch arm position
+            return pureSequentialSequence;
+        }
+
+        if (elevatorUp && armUp && !targetElevatorUp && !targetArmUp) {     // Elevator all the way up and wants to kah-chunk.
+            return topToBottomSequence;
+        }
+
+        if (!elevatorUp && !armUp && targetElevatorUp && targetArmUp) {     // Elevator kah-chunked and wants to go all the way up.            
+            return bottomToTopSequence;
+        }
+
+        // This sequence is least stable and is only used if there is no chance that the arm will hit the kahchunk block.
+        return pureParallelSequence;
     }
 }
