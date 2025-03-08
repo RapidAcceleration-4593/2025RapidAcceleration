9731164a53d238cae0f23cfa19442a759ded35cf
TylerMueller08 <tylerjacobmueller08@gmail.com>
2025-03-08 16:24:30 -0700

    Importing MAX_TRAVEL_TIME for Elevator and Arm in KahChunkCommand.

--- src/main/java/frc/robot/commands/armivator/KahChunkCommand.java
+++ src/main/java/frc/robot/commands/armivator/KahChunkCommand.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.RobotStates.Arm.ArmStates;
 import frc.robot.Constants.RobotStates.Elevator.ElevatorStates;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.ArmConstants;
 import frc.robot.commands.arm.SetArmState;
 import frc.robot.commands.elevator.SetElevatorState;
 import frc.robot.subsystems.ArmSubsystem;
@@ -14,11 +16,11 @@ public class KahChunkCommand extends SequentialCommandGroup {
     public KahChunkCommand(ElevatorSubsystem elevatorSubsystem, ArmSubsystem armSubsystem) {
         addCommands(
             Commands.parallel(
-                new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(1.25),
-                new SetArmState(armSubsystem, ArmStates.BOTTOM).withTimeout(1.5)
+                new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(ElevatorConstants.MAX_TRAVEL_TIME),
+                new SetArmState(armSubsystem, ArmStates.BOTTOM).withTimeout(ArmConstants.MAX_TRAVEL_TIME)
             ),
-            new SetElevatorState(elevatorSubsystem, ElevatorStates.BOTTOM).withTimeout(0.8),
-            new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(0.8)
+            new SetElevatorState(elevatorSubsystem, ElevatorStates.BOTTOM).withTimeout(ElevatorConstants.MAX_TRAVEL_TIME),
+            new SetElevatorState(elevatorSubsystem, ElevatorStates.PICKUP).withTimeout(ElevatorConstants.MAX_TRAVEL_TIME)
         );
     }
 }
